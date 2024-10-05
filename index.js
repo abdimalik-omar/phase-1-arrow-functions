ec0894c7156d36563c47c7460c3ba6d3424e266c
abdimalik-omar <abdimalik.omar1@student.moringaschool.com>
2024-10-05 23:49:57 +0300

    Completed lab

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1. Simple arrow function
+const divide = (a, b) => a / b;
+console.log(divide(2000, 100)); // Output: 8
+
+// 2. Arrow function with array
+const square = (h) => h * h;
+console.log(square(5)); 
+
+const add = (c, d) => c + d;
+console.log(add(18, 9));
+
+
+
